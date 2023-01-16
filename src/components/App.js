a02edf2961048e06f674257b8bf4d4041e38b7b9
Charbel Maroun <charbel-maroun@hotmail.com>
2023-01-15 19:31:58 -0500

    commit tree dc755d496c83a2ba8ca6ed31aedd363de22d1ed0

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then(res => res.json())
+      .then(list => setList(list))
+  }, [])
   const [page, setPage] = useState("List");
+
+  const handleFormSubmit = (formData) =>{
+    setList([...list, formData])
+  }
+
+  const handleDeleteQuestion = (deletedQuestion) => {
+    const updatedQuestionList = list.filter(listItem => listItem.id !== deletedQuestion.id);
+    setList(updatedQuestionList)
+  }
+const handleChangeAnswer = () =>{
+  setList(list)
+}
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm onFormSubmit={handleFormSubmit} /> : <QuestionList list={list} onDeleteClick={handleDeleteQuestion} onChangeClick={handleChangeAnswer}/>}
     </main>
   );
 }
